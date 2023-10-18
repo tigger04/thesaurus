cd1809c05b19919ba9168dec23f812a66616f68c
tigger <tigger04@gmail.com>
2023-10-18 15:27:11 +0100

    nospoon:acp

--- thes.pl
+++ thes.pl
@@ -12,13 +12,21 @@ open(my $ifh, "look \"$arg\" $thesaurus |") || die "look $thesaurus";
 
 while (<$ifh>) {
     chomp;
-    @arr = split(/,/, $_);
-    print "$arr[0]\n";
-    shift(@arr);
-    open(my $pfh, "| pr -3t -w85 | expand") || die "pr";
-    foreach (@arr) { print $pfh "  $_\n"; }
-    print $pfh "\n";
-    close($pfh);
+    # @arr = split(/,/, $_);
+    # print "$arr[0]\n";
+    # shift(@arr);
+    # open(my $pfh, "| pr -3t -w85 | expand") || die "pr";
+    # foreach (@arr) { print $pfh "  $_\n"; }
+    # print $pfh "\n";
+    # close($pfh);
+    my @synonyms = split(/,/, $_);
+    print "\n---\U$synonyms[0]---\n";
+    
+    shift(@synonyms);  # Discard the word being looked up
+
+    for my $synonym (@synonyms) {
+        print "$synonym\n";
+    }
 }
 
 close($ifh);
